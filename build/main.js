b3b969927bc4191c6382727da8112a567671e572
SosukeIto <63573845+SosukeIto@users.noreply.github.com>
2025-03-09 21:21:17 +0900

    update: ボタンの配置に関するプログラムの修正

--- build/main.js
+++ build/main.js
@@ -42,59 +42,14 @@ client.on(discord_js_1.Events.InteractionCreate, (interaction) => __awaiter(void
         return;
     if (interaction.commandName === 'button') {
         // ボタンの作成
-        const buttons = (0, generateButton_1.generateButton)(["1", "2", "3", "4"]);
-        const button1 = new discord_js_1.ButtonBuilder()
-            .setCustomId('test_button1')
-            .setLabel('クリック！')
-            .setStyle(discord_js_1.ButtonStyle.Primary);
-        const button2 = new discord_js_1.ButtonBuilder()
-            .setCustomId('test_button2')
-            .setLabel('クリック！')
-            .setStyle(discord_js_1.ButtonStyle.Primary);
-        const button3 = new discord_js_1.ButtonBuilder()
-            .setCustomId('test_button3')
-            .setLabel('クリック！')
-            .setStyle(discord_js_1.ButtonStyle.Primary);
-        const button4 = new discord_js_1.ButtonBuilder()
-            .setCustomId('test_button4')
-            .setLabel('クリック！')
-            .setStyle(discord_js_1.ButtonStyle.Primary);
-        const button5 = new discord_js_1.ButtonBuilder()
-            .setCustomId('test_button5')
-            .setLabel('クリック！')
-            .setStyle(discord_js_1.ButtonStyle.Primary);
-        const button6 = new discord_js_1.ButtonBuilder()
-            .setCustomId('test_button6')
-            .setLabel('クリック！')
-            .setStyle(discord_js_1.ButtonStyle.Primary);
-        const button7 = new discord_js_1.ButtonBuilder()
-            .setCustomId('test_button7')
-            .setLabel('クリック！')
-            .setStyle(discord_js_1.ButtonStyle.Primary);
-        const button8 = new discord_js_1.ButtonBuilder()
-            .setCustomId('test_button8')
-            .setLabel('クリック！')
-            .setStyle(discord_js_1.ButtonStyle.Primary);
-        const button9 = new discord_js_1.ButtonBuilder()
-            .setCustomId('test_button9')
-            .setLabel('クリック！')
-            .setStyle(discord_js_1.ButtonStyle.Primary);
-        const button10 = new discord_js_1.ButtonBuilder()
-            .setCustomId('test_button10')
-            .setLabel('クリック！')
-            .setStyle(discord_js_1.ButtonStyle.Primary);
-        const button11 = new discord_js_1.ButtonBuilder()
-            .setCustomId('test_button11')
-            .setLabel('クリック！')
-            .setStyle(discord_js_1.ButtonStyle.Primary);
-        const button12 = new discord_js_1.ButtonBuilder()
-            .setCustomId('test_button12')
-            .setLabel('クリック！')
-            .setStyle(discord_js_1.ButtonStyle.Primary);
-        const row1 = new discord_js_1.ActionRowBuilder().addComponents(button1, button2, button3, button4);
-        const row2 = new discord_js_1.ActionRowBuilder().addComponents(button5, button6, button7, button8);
-        const row3 = new discord_js_1.ActionRowBuilder().addComponents(button9, button10, button11, button12);
-        const row4 = new discord_js_1.ActionRowBuilder().addComponents(buttons[0], buttons[1], buttons[2], buttons[3]);
+        const buttons1 = (0, generateButton_1.generateButton)(["1", "2", "3", "4"]);
+        const buttons2 = (0, generateButton_1.generateButton)(["5", "6", "7", "8"]);
+        const buttons3 = (0, generateButton_1.generateButton)(["9", "10", "11", "12"]);
+        const buttons4 = (0, generateButton_1.generateButton)(["13", "14", "15", "16"]);
+        const row1 = new discord_js_1.ActionRowBuilder().addComponents(buttons1);
+        const row2 = new discord_js_1.ActionRowBuilder().addComponents(buttons2);
+        const row3 = new discord_js_1.ActionRowBuilder().addComponents(buttons3);
+        const row4 = new discord_js_1.ActionRowBuilder().addComponents(buttons4);
         yield interaction.reply({ content: 'ターミナル1', components: [row1, row2, row3, row4] });
     }
 }));
--- src/main.ts
+++ src/main.ts
@@ -35,59 +35,14 @@ client.on(Events.InteractionCreate, async (interaction) => {
 
     if (interaction.commandName === 'button') {
         // ボタンの作成
-        const buttons = generateButton(["1", "2", "3", "4"])
-        const button1: ButtonBuilder = new ButtonBuilder()
-            .setCustomId('test_button1')
-            .setLabel('クリック！')
-            .setStyle(ButtonStyle.Primary);
-        const button2: ButtonBuilder = new ButtonBuilder()
-            .setCustomId('test_button2')
-            .setLabel('クリック！')
-            .setStyle(ButtonStyle.Primary);
-        const button3: ButtonBuilder = new ButtonBuilder()
-            .setCustomId('test_button3')
-            .setLabel('クリック！')
-            .setStyle(ButtonStyle.Primary);
-        const button4: ButtonBuilder = new ButtonBuilder()
-            .setCustomId('test_button4')
-            .setLabel('クリック！')
-            .setStyle(ButtonStyle.Primary);
-        const button5: ButtonBuilder = new ButtonBuilder()
-            .setCustomId('test_button5')
-            .setLabel('クリック！')
-            .setStyle(ButtonStyle.Primary);
-        const button6: ButtonBuilder = new ButtonBuilder()
-            .setCustomId('test_button6')
-            .setLabel('クリック！')
-            .setStyle(ButtonStyle.Primary);
-        const button7: ButtonBuilder = new ButtonBuilder()
-            .setCustomId('test_button7')
-            .setLabel('クリック！')
-            .setStyle(ButtonStyle.Primary);
-        const button8: ButtonBuilder = new ButtonBuilder()
-            .setCustomId('test_button8')
-            .setLabel('クリック！')
-            .setStyle(ButtonStyle.Primary);
-        const button9: ButtonBuilder = new ButtonBuilder()
-            .setCustomId('test_button9')
-            .setLabel('クリック！')
-            .setStyle(ButtonStyle.Primary);
-        const button10: ButtonBuilder = new ButtonBuilder()
-            .setCustomId('test_button10')
-            .setLabel('クリック！')
-            .setStyle(ButtonStyle.Primary);
-        const button11: ButtonBuilder = new ButtonBuilder()
-            .setCustomId('test_button11')
-            .setLabel('クリック！')
-            .setStyle(ButtonStyle.Primary);
-        const button12: ButtonBuilder = new ButtonBuilder()
-            .setCustomId('test_button12')
-            .setLabel('クリック！')
-            .setStyle(ButtonStyle.Primary);
-        const row1 = new ActionRowBuilder<ButtonBuilder>().addComponents(button1, button2, button3, button4);
-        const row2 = new ActionRowBuilder<ButtonBuilder>().addComponents(button5, button6, button7, button8);
-        const row3 = new ActionRowBuilder<ButtonBuilder>().addComponents(button9, button10, button11, button12);
-        const row4 = new ActionRowBuilder<ButtonBuilder>().addComponents(buttons[0], buttons[1], buttons[2], buttons[3]);
+        const buttons1 = generateButton(["1", "2", "3", "4"])
+        const buttons2 = generateButton(["5", "6", "7", "8"])
+        const buttons3 = generateButton(["9", "10", "11", "12"])
+        const buttons4 = generateButton(["13", "14", "15", "16"])
+        const row1 = new ActionRowBuilder<ButtonBuilder>().addComponents(buttons1);
+        const row2 = new ActionRowBuilder<ButtonBuilder>().addComponents(buttons2);
+        const row3 = new ActionRowBuilder<ButtonBuilder>().addComponents(buttons3);
+        const row4 = new ActionRowBuilder<ButtonBuilder>().addComponents(buttons4);
         await interaction.reply({ content: 'ターミナル1', components: [row1, row2, row3, row4] });
     }
 });
