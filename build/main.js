40d32cf359bcaaaec009eb9c5b5e869ce71147d4
SosukeIto <63573845+SosukeIto@users.noreply.github.com>
2025-03-16 21:18:31 +0900

    delete: 不要な機能の削除

--- build/main.js
+++ build/main.js
@@ -13,18 +13,12 @@ const discord_js_1 = require("discord.js");
 const rest_1 = require("@discordjs/rest");
 const v10_1 = require("discord-api-types/v10");
 require("dotenv/config");
-const generateButton_1 = require("./modules/generateButton");
-const generateDevice_1 = require("./modules/generateDevice");
-const generateComponents_1 = require("./modules/generateComponents");
 const generateMatchColorsComponents_1 = require("./modules/generateMatchColorsComponents");
 const generateChargeEnergy_1 = require("./modules/generateChargeEnergy");
 const client = new discord_js_1.Client({ intents: [discord_js_1.GatewayIntentBits.Guilds] });
 const TOKEN = process.env.DISCORD_TOKEN;
 const CLIENT_ID = process.env.CLIENT_ID;
 const GUILD_ID = process.env.GUILD_ID;
-let customIds = [];
-let deviceCustomIds = [];
-let deviceButtons = [];
 const terminalRoute = [
     { "t00": 1 }, { "t01": 2 }, { "t02": 3 }, { "t03": 4 }, { "t04": 5 },
     { "t10": 5 }, { "t11": 1 }, { "t12": 2 }, { "t13": 3 }, { "t14": 4 },
@@ -44,18 +38,7 @@ const panelChargeEnergy = [
     { "t30": 3 }, { "t31": 1 }, { "t32": -1 }, { "t33": 2 }, { "t34": 1 },
     { "t40": -2 }, { "t41": -1 }, { "t42": -1 }, { "t43": -1 }, { "t44": -3 },
 ];
-let buttonMap;
-// スラッシュコマンドの登録
 const commands = [
-    new discord_js_1.SlashCommandBuilder()
-        .setName('button')
-        .setDescription('ボタンを表示します。'),
-    new discord_js_1.SlashCommandBuilder()
-        .setName('device')
-        .setDescription('デバイス起動'),
-    new discord_js_1.SlashCommandBuilder()
-        .setName('terminal')
-        .setDescription('デバイス起動'),
     new discord_js_1.SlashCommandBuilder()
         .setName('match-colors')
         .setDescription('色を揃えろ！'),
@@ -78,43 +61,6 @@ const rest = new rest_1.REST({ version: '10' }).setToken(TOKEN);
 client.on(discord_js_1.Events.InteractionCreate, (interaction) => __awaiter(void 0, void 0, void 0, function* () {
     if (!interaction.isChatInputCommand())
         return;
-    if (interaction.commandName === 'button') {
-        // ボタンの作成
-        const [buttons1, customIds1] = (0, generateButton_1.generateButton)(["1", "2", "3", "4"]);
-        const [buttons2, customIds2] = (0, generateButton_1.generateButton)(["5", "6", "7", "8"]);
-        const [buttons3, customIds3] = (0, generateButton_1.generateButton)(["9", "10", "11", "12"]);
-        const [buttons4, customIds4] = (0, generateButton_1.generateButton)(["13", "14", "15", "16"]);
-        const row1 = new discord_js_1.ActionRowBuilder().addComponents(buttons1);
-        const row2 = new discord_js_1.ActionRowBuilder().addComponents(buttons2);
-        const row3 = new discord_js_1.ActionRowBuilder().addComponents(buttons3);
-        const row4 = new discord_js_1.ActionRowBuilder().addComponents(buttons4);
-        yield interaction.reply({ content: 'ターミナル1', components: [row1, row2, row3, row4] });
-        customIds = [...customIds1, ...customIds2, ...customIds3, ...customIds4];
-    }
-    if (interaction.commandName === "device") {
-        const deviceRoute = [
-            [-1, -1, -1, -1, -1],
-            [-1, 1, 2, 3, -1],
-            [-1, 1, -1, 1, -1],
-            [1, 1, -1, 3, 1],
-            [-2, -1, -1, -1, -3]
-        ];
-        const components = [];
-        for (const route of deviceRoute) {
-            const [buttons, customId] = (0, generateDevice_1.generateDevice)(route);
-            const row = new discord_js_1.ActionRowBuilder().addComponents(buttons);
-            components.push(row);
-            deviceCustomIds.push(...customId);
-            deviceButtons.push(...buttons);
-        }
-        yield interaction.reply({ content: 'ターミナル1', components: components });
-        buttonMap = Object.fromEntries(deviceCustomIds.map((item, index) => [item, deviceRoute.flat()[index]]));
-        console.log(buttonMap);
-    }
-    if (interaction.commandName === "terminal") {
-        const components = (0, generateComponents_1.generateComponents)(terminalRoute);
-        yield interaction.reply({ content: 'ターミナル1', components: components });
-    }
     if (interaction.commandName === "match-colors") {
         const components = (0, generateMatchColorsComponents_1.generateMatchColorsComponents)(panelMatchColors);
         yield interaction.reply({ content: '色を揃えろ！', components: components });
@@ -128,19 +74,7 @@ client.on(discord_js_1.Events.InteractionCreate, (interaction) => __awaiter(void
 client.on(discord_js_1.Events.InteractionCreate, (interaction) => __awaiter(void 0, void 0, void 0, function* () {
     if (!interaction.isButton())
         return;
-    /*
-        if (interaction.customId in buttonMap) {
-            await interaction.reply({ content: `${buttonMap[interaction.customId]}`, ephemeral: true });
-        }
-    */
     yield interaction.deferUpdate();
-    for (let i = 0; i < terminalRoute.length; i++) {
-        if (Object.keys(terminalRoute[i])[0] === interaction.customId) {
-            terminalRoute[i][interaction.customId]--;
-            const components = (0, generateComponents_1.generateComponents)(terminalRoute);
-            yield interaction.editReply({ content: 'ターミナル1', components: components });
-        }
-    }
     for (let i = 0; i < panelMatchColors.length; i++) {
         if (Object.keys(panelMatchColors[i])[0] === interaction.customId) {
             panelMatchColors[i][interaction.customId]--;
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,14 @@
-import { Client, GatewayIntentBits, SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, Events, ButtonStyle } from 'discord.js';
+import { Client, GatewayIntentBits, SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, Events } from 'discord.js';
 import { REST } from '@discordjs/rest';
 import { Routes } from 'discord-api-types/v10';
 import 'dotenv/config';
-import { generateButton } from './modules/generateButton';
-import { generateDevice } from './modules/generateDevice';
-import { generateComponents } from './modules/generateComponents'
 import { generateMatchColorsComponents } from './modules/generateMatchColorsComponents';
 import { generateChargeEnergy } from './modules/generateChargeEnergy';
+
 const client: Client = new Client({ intents: [GatewayIntentBits.Guilds] });
 const TOKEN: string = process.env.DISCORD_TOKEN!;
 const CLIENT_ID: string = process.env.CLIENT_ID!;
 const GUILD_ID: string = process.env.GUILD_ID!;
-let customIds: string[] = [];
-let deviceCustomIds: string[] = [];
-let deviceButtons: ButtonBuilder[] = [];
 const terminalRoute: Record<string, number>[]= [
     {"t00":1}, {"t01":2}, {"t02":3}, {"t03":4}, {"t04":5},
     {"t10":5}, {"t11":1}, {"t12":2}, {"t13":3}, {"t14":4},
@@ -33,18 +28,8 @@ const panelChargeEnergy: Record<string, number>[]= [
     {"t30":3}, {"t31":1}, {"t32":-1}, {"t33":2}, {"t34":1},
     {"t40":-2}, {"t41":-1}, {"t42":-1}, {"t43":-1}, {"t44":-3},
 ];
-let buttonMap: Record<string, number>;
-// スラッシュコマンドの登録
+
 const commands = [
-    new SlashCommandBuilder()
-        .setName('button')
-        .setDescription('ボタンを表示します。'),
-    new SlashCommandBuilder()
-        .setName('device')
-        .setDescription('デバイス起動'),
-    new SlashCommandBuilder()
-        .setName('terminal')
-        .setDescription('デバイス起動'),
     new SlashCommandBuilder()
         .setName('match-colors')
         .setDescription('色を揃えろ！'),
@@ -68,46 +53,6 @@ const rest: REST = new REST({ version: '10' }).setToken(TOKEN);
 // コマンド実行時の処理
 client.on(Events.InteractionCreate, async (interaction) => {
     if (!interaction.isChatInputCommand()) return;
-
-    if (interaction.commandName === 'button') {
-        // ボタンの作成
-        const [ buttons1, customIds1 ] = generateButton(["1", "2", "3", "4"]);
-        const [ buttons2, customIds2 ] = generateButton(["5", "6", "7", "8"]);
-        const [ buttons3, customIds3 ] = generateButton(["9", "10", "11", "12"]);
-        const [ buttons4, customIds4 ] = generateButton(["13", "14", "15", "16"]);
-        const row1 = new ActionRowBuilder<ButtonBuilder>().addComponents(buttons1);
-        const row2 = new ActionRowBuilder<ButtonBuilder>().addComponents(buttons2);
-        const row3 = new ActionRowBuilder<ButtonBuilder>().addComponents(buttons3);
-        const row4 = new ActionRowBuilder<ButtonBuilder>().addComponents(buttons4);
-        await interaction.reply({ content: 'ターミナル1', components: [row1, row2, row3, row4] });
-        customIds = [...customIds1, ...customIds2, ...customIds3, ...customIds4];
-    }
-    if(interaction.commandName === "device"){
-        const deviceRoute = [
-            [-1, -1, -1, -1, -1],
-            [-1, 1, 2, 3, -1],
-            [-1, 1, -1, 1, -1],
-            [1, 1, -1, 3, 1],
-            [-2, -1, -1, -1, -3]
-        ];
-        const components: ActionRowBuilder<ButtonBuilder>[]= []
-        for(const route of deviceRoute){
-            const [ buttons, customId ] = generateDevice(route);
-            const row: ActionRowBuilder<ButtonBuilder>= new ActionRowBuilder<ButtonBuilder>().addComponents(buttons);
-            components.push(row);
-            deviceCustomIds.push(...customId);
-            deviceButtons.push(...buttons);
-        }
-        await interaction.reply({ content: 'ターミナル1', components: components});
-        buttonMap = Object.fromEntries(
-            deviceCustomIds.map((item, index) => [item, deviceRoute.flat()[index]])
-        );
-        console.log(buttonMap);
-    }
-    if(interaction.commandName === "terminal"){
-        const components: ActionRowBuilder<ButtonBuilder>[] = generateComponents(terminalRoute);
-        await interaction.reply({ content: 'ターミナル1', components: components});
-    }
     if(interaction.commandName === "match-colors"){
         const components: ActionRowBuilder<ButtonBuilder>[] = generateMatchColorsComponents(panelMatchColors);
         await interaction.reply({ content: '色を揃えろ！', components: components});
@@ -121,19 +66,8 @@ client.on(Events.InteractionCreate, async (interaction) => {
 // ボタンクリック時の処理
 client.on(Events.InteractionCreate, async (interaction) => {
     if (!interaction.isButton()) return;
-/*
-    if (interaction.customId in buttonMap) {
-        await interaction.reply({ content: `${buttonMap[interaction.customId]}`, ephemeral: true });
-    }
-*/
+
     await interaction.deferUpdate(); 
-    for(let i = 0; i < terminalRoute.length; i++){
-        if(Object.keys(terminalRoute[i])[0] === interaction.customId){
-            terminalRoute[i][interaction.customId]--;
-            const components: ActionRowBuilder<ButtonBuilder>[] = generateComponents(terminalRoute);
-            await interaction.editReply({ content: 'ターミナル1', components: components});
-        }
-    }
     for(let i = 0; i < panelMatchColors.length; i++){
         if(Object.keys(panelMatchColors[i])[0] === interaction.customId){
             panelMatchColors[i][interaction.customId]--;
